972310e5ea3544dbbb3c1b6d1e5dc058c2b212d0
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-30 15:43:44 -0300

    Exercise 11

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './StrictAcess';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router';
 // import { Link } from 'react-router-dom';
 import { Switch } from 'react-router';
 class App extends Component {
   render() {
+    const user = { username: 'joao', password: '1234' };
     return (
       <BrowserRouter>
         {/* <Link to="/">Home</Link>
@@ -15,6 +17,7 @@ class App extends Component {
         <Link to="/users">Users</Link> */}
         <Switch>
           <Route path="/users/:id" render={ (props) => <Users {...props} /> } />
+          <Route path="/strict-access" render={ () => <StrictAccess user={ user }  /> } />
           <Route path="/about" component={About} />
           <Route path="/" component={Home} />
         </Switch>
